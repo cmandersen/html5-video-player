e2b75705366c5285f32adf3e1d51e77e06cef84b
Christian Morgan Andersen <cmandersen@outlook.com>
2016-05-20 07:16:15 +0200

    Initial commit

---
+++ html5-video-player.js
@@ -0,0 +1,12 @@
+function CMAVideo(element, options) {
+    this.player = element;
+    this.options = options;
+
+    this.init();
+}
+
+CMAVideo.prototype = {
+    init : function() {
+        
+    }
+};
